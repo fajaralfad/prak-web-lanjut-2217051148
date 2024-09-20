6a9e2f0a1016735bcdd2823c5c17ea72ccc8685c
fajaralfad <fajaralfad85@gmail.com>
2024-09-20 21:03:34 +0700

    pertemuan 3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/profile/{nama}/{kelas}/{npm}',
-[ProfileController::class, 'profile']);
+Route::get('/user/profile', [UserController::class, 'profile']);
 
+Route::get('/user/create', [UserController::class,
+'create']);
 
-
+Route::post('/user/store', [UserController::class,
+'store'])->name('user.store');
